7a459cef48b75cea35676235113cc4c2379e7a93
Gabriel Ayala <gabeayala100@gmail.com>
2021-08-13 09:52:56 -0700

    Add the functionality for the button.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-/*import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import any business logic with the following syntax:
-import [BusinessObject] from './triangle.js';
-for multiple imports from the same file follow this syntax:
-import[{*/
+
+$(document).ready(function() {
+  $('#exchangeRate').click(function() {
+    const currency = $('#userConversion').val();
+    const base = $('#userBase').val();
+    $('#userBase').val('');
+    $('#userCurrency').val('');
+    
+
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${base}/${currency}`
+    })
+  });
+});
 
 
 
