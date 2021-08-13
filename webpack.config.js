172ee6405ab78329c99921d1e6d9fe0503259fc3
Gabriel Ayala <gabeayala100@gmail.com>
2021-08-13 08:51:09 -0700

    Change all template names to current project.

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new Dotenv(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'template-repo', // change title 
+      title: 'Exchange-Rate',
       template: './src/index.html',
       inject: 'body'
     })
