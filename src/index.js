95c6d64d7c3921521d7036e073140bea74bc77a5
Gabriel Ayala <gabeayala100@gmail.com>
2021-08-13 11:00:34 -0700

    transfer promise and update the .then functionality.

--- src/index.js
+++ src/index.js
@@ -2,40 +2,27 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyConversion from './currency-converter.js';
 
+function clearFields() {
+  $('#userBase').val('');
+  $('#userConversion').val('');
+  $('#userAmount').val('');
+}
 $(document).ready(function() {
   $('#exchangeRate').click(function() {
+    const base = $('#userBase').val();
     const currency = $('#userConversion').val();
     const monetaryValue = parseInt($('#userAmount').val());
-
-
-    const base = $('#userBase').val();
-    $('#userBase').val('');
-    $('#userConversion').val('');
-    $('#userAmount').val('');
-    
-
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${base}/${currency}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
-
+    clearFields();
+    let promise = CurrencyConversion.getConversion(base,currency);
     promise.then(function(response) {
       const body = JSON.parse(response);
       const parseIntConversion = parseFloat(`${body.conversion_rate}`);
-      console.log(parseIntConversion);
       const finalConvert = (`${parseIntConversion}` * `${monetaryValue}`);
-      console.log(typeof finalConvert);
       $('.showConversion').text(`${finalConvert}`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
     });
   });
 });
