b150012adc83489368609ea2f31559134e75fc93
Gabriel Ayala <gabeayala100@gmail.com>
2021-08-13 09:57:29 -0700

    add a promise.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,21 @@ $(document).ready(function() {
 
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${base}/${currency}`
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${base}/${currency}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showConversion')
     })
   });
 });
