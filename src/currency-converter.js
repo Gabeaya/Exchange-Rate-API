6e697a8ffbf11532725b755527d54b6148eac63c
Gabriel Ayala <gabeayala100@gmail.com>
2021-08-13 10:59:52 -0700

    Add the promise to a static method here.

---
+++ src/currency-converter.js
@@ -0,0 +1,17 @@
+export default class CurrencyConversion {
+  static getConversion(base, currency) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${base}/${currency}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
